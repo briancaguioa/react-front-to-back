3e8ae25353d5cd7d40c63b668193acbf7022d429
Brian <brian.gcaguioa@gmail.com>
2021-10-22 12:47:30 +0800

    make prettier default formatter

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,109 +1,108 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import TextInputGroup from '../layout/TextInputGroup';
-import {v1 as uuid} from "uuid"; 
+import { v1 as uuid } from 'uuid';
 
 class AddContact extends Component {
-  state = {
-    name: '',
-    email: '',
-    phone: '',
-    errors: {}
-  }
+	state = {
+		name: '',
+		email: '',
+		phone: '',
+		errors: {}
+	};
 
-  onSubmit = (dispatch, e) => {
-    e.preventDefault();
+	onSubmit = (dispatch, e) => {
+		e.preventDefault();
 
-    const { name, email, phone } = this.state;
+		const { name, email, phone } = this.state;
 
-    if(name === '') {
-      this.setState({errors: {name: 'Name is required'}});
-      return;
-    }
+		if (name === '') {
+			this.setState({ errors: { name: 'Name is required' } });
+			return;
+		}
 
-    if(email === '') {
-      this.setState({errors: {email: 'Email is required'}});
-      return;
-    }
+		if (email === '') {
+			this.setState({ errors: { email: 'Email is required' } });
+			return;
+		}
 
-    if(phone === '') {
-      this.setState({errors: {phone: 'Phone is required'}});
-      return;
-    }
+		if (phone === '') {
+			this.setState({ errors: { phone: 'Phone is required' } });
+			return;
+		}
 
-    const newContact = {
-      id: uuid(), 
-      name, 
-      email, 
-      phone
-    }
+		const newContact = {
+			id: uuid(),
+			name,
+			email,
+			phone
+		};
 
-    dispatch({type: 'ADD_CONTACT', payload: newContact});
+		dispatch({ type: 'ADD_CONTACT', payload: newContact });
 
-    // clear state
-    this.setState({
-      name: '',
-      email: '',
-      phone: '',
-      errors: {}
-    });
+		// clear state
+		this.setState({
+			name: '',
+			email: '',
+			phone: '',
+			errors: {}
+		});
 
-    this.props.history.push('/');
+		this.props.history.push('/');
+	};
 
-  }
+	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value});
+	render() {
+		const { name, email, phone, errors } = this.state;
 
-  render() {
-    const { name, email, phone, errors } = this.state;
-
-    return (
-      <Consumer>
-        {value => {
-          const { dispatch } = value;
-          return (
-            <div className="card mb-3">
-              <div className="card-header">Add Contact</div>
-              <div className="card-body">
-                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <TextInputGroup 
-                    label = "Name"
-                    name = "name"
-                    placeholder = "Enter Name..."
-                    value = {name}
-                    onChange = {this.onChange}
-                    error = {errors.name}
-                  />
-                  <TextInputGroup 
-                    label = "Email"
-                    name = "email"
-                    type = "email"
-                    placeholder = "Enter Email..."
-                    value = {email}
-                    onChange = {this.onChange}
-                    error = {errors.email}
-                  />
-                  <TextInputGroup 
-                    label = "Phone"
-                    name = "phone"
-                    placeholder = "Enter Number..."
-                    value = {phone}
-                    onChange = {this.onChange}
-                    error = {errors.phone}
-                  />
-                  <input 
-                    type="submit" 
-                    value="Add Contact" 
-                    className="form-control btn btn-primary btn-light mt-3"
-                  />
-                </form>
-              </div>
-            </div>
-          )
-        }}
-      </Consumer>
-    )
-  }
+		return (
+			<Consumer>
+				{(value) => {
+					const { dispatch } = value;
+					return (
+						<div className="card mb-3">
+							<div className="card-header">Add Contact</div>
+							<div className="card-body">
+								<form onSubmit={this.onSubmit.bind(this, dispatch)}>
+									<TextInputGroup
+										label="Name"
+										name="name"
+										placeholder="Enter Name..."
+										value={name}
+										onChange={this.onChange}
+										error={errors.name}
+									/>
+									<TextInputGroup
+										label="Email"
+										name="email"
+										type="email"
+										placeholder="Enter Email..."
+										value={email}
+										onChange={this.onChange}
+										error={errors.email}
+									/>
+									<TextInputGroup
+										label="Phone"
+										name="phone"
+										placeholder="Enter Number..."
+										value={phone}
+										onChange={this.onChange}
+										error={errors.phone}
+									/>
+									<input
+										type="submit"
+										value="Add Contact"
+										className="form-control btn btn-primary btn-light mt-3"
+									/>
+								</form>
+							</div>
+						</div>
+					);
+				}}
+			</Consumer>
+		);
+	}
 }
 
-export default AddContact
+export default AddContact;
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -3,28 +3,23 @@ import Contact from './Contact';
 import { Consumer } from '../../context';
 
 class Contacts extends Component {
-  render() {
-    return (
-      <Consumer>
-        {value => {
-          const {contacts} = value;
-          return (
-            <Fragment>
-              <h1 className="display-4 mb-2">
-                <span className="text-danger">Contact</span> List
-              </h1>
-              {contacts.map(contact => 
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                />
-              )}
-            </Fragment>
-          )
-        }}
-      </Consumer>
-    )
-  }
+	render() {
+		return (
+			<Consumer>
+				{(value) => {
+					const { contacts } = value;
+					return (
+						<Fragment>
+							<h1 className="display-4 mb-2">
+								<span className="text-danger">Contact</span> List
+							</h1>
+							{contacts.map((contact) => <Contact key={contact.id} contact={contact} />)}
+						</Fragment>
+					);
+				}}
+			</Consumer>
+		);
+	}
 }
 
-export default Contacts
+export default Contacts;
